eced5d2c1252c0562ce03a3790808402ef456495
PAVEL RING <167412581+Odrozkeskal@users.noreply.github.com>
2024-09-27 17:54:39 +0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -19,11 +19,14 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
+      host: process.env.DB_HOST, 
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       ssl: { rejectUnauthorized: false }
     },
     migrations: {
       tableName: 'knex_migrations'
-    },
+    }
   },
 };
